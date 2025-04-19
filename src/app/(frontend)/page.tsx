4c71cf698cc7f89734936ac531d6743e2d7a1ee0
didiraja <didiraja@hotmail.com>
2025-04-18 21:22:03 -0300

    style: 💄 design tweaks

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -22,15 +22,6 @@ export default async function HomePage() {
   return (
     <div className="home">
       <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
         <h1>My Coins</h1>
         <div className="links">
           <a
