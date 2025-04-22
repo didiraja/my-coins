5f2a2e43eba483f8f339f21d7aca6e1ff63d5372
Dico Didiraja <didiraja@hotmail.com>
2025-04-22 13:39:05 -0300

    Update request.ts

--- src/libs/request.ts
+++ src/libs/request.ts
@@ -39,7 +39,7 @@ export type IDashEndpoint = {
 
 export const GetDashValues = async () => {
   try {
-    const response: AxiosResponse<IDashEndpoint> = await axios.get(`${process.env.VERCEL_ENV === "production" ? ("https://"+process.env.VERCEL_URL) : "http://localhost:3000"}/api/dashboard`,
+    const response: AxiosResponse<IDashEndpoint> = await axios.get(`${process.env.VERCEL_ENV === "production" ? "https://my-coins-vert.vercel.app/" : "http://localhost:3000"}/api/dashboard`,
       {
         headers: {
           'x-secret-token': process.env.PAYLOAD_SECRET,
