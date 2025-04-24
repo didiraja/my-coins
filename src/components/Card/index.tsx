97cf58f1de935a914850a25f346781ee8cd08f21
didiraja <didiraja@hotmail.com>
2025-04-24 18:53:45 -0300

    style: 💄 apply tailwind classes directly

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,11 +2,19 @@
 
 import S from './Card.module.css'
 
-const Card = ({ label, children }: { label: string; children: string }) => {
+const Card = ({
+  label,
+  children,
+  profit,
+}: {
+  label: string
+  children: string
+  profit?: boolean
+}) => {
   return (
-    <div className={S.card}>
-      <p className={S.label}>{label}</p>
-      <p className={S.value}>{children}</p>
+    <div className="bg-white text-black rounded-md p-2 text-xl">
+      <p className="font-semibold">{label}</p>
+      <p className={profit ? 'text-green-500' : 'text-red-500'}>{children}</p>
     </div>
   )
 }
